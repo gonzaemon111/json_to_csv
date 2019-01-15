61a4de33bfdd759b64d592fe863d63a00eafb69b
gomzaemon111 <goki14914@icloud.com>
2019-01-15 17:42:21 +0900

    assign some files

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
 require "json"
 require "csv"
 
+json_file_path = "json/ase/ase_2.json"
+csv_file_path = "csv/ase/ase_2.csv"
+
+CSV.open(csv_file_path, "w") do |csv| #open new file for write
+  JSON.parse(File.open(json_file_path).read).each do |hash| #open json to parse
+    #write value to file
+    csv << hash.values
+    puts hash.keys
+  end
+end
