7f6b529af3f8a7e7bccc0c0006ca828d8bdbe0fe
gomzaemon111 <goki14914@icloud.com>
2019-01-18 18:18:28 +0900

    assign perfect main.rb

--- main2.rb
+++ main2.rb
@@ -1,11 +1,14 @@
 require "json"
 require "csv"
 
-csv_file_path = "csv/ase/ase_1.csv"
+csv_file_path = "csv/fse/fse_6.csv"
 
 def insert_element(value, term, i, j)
   # i = 通年かどうかで決まる 基本的に0のみ
   # j = その期で授業コマ文だけ回すため
+  day = !value[19][0].empty? ? value[19][i][j]["day"] : "なし"
+  period = !value[19][0].empty? ? value[19][i][j]["period"] : "なし"
+  classroom = !value[19][0].empty? ? value[19][i][j]["classroom"] : "なし"
   [].push( value[0] ,
            value[1] ,
            value[2] ,
@@ -25,33 +28,44 @@ def insert_element(value, term, i, j)
            value[16] ,
            value[17] ,
            term ,
-           value[19][i][j]["day"] ,
-           value[19][i][j]["period"] ,
-           value[19][i][j]["classroom"],
+           day ,
+           period ,
+           classroom,
            value[20] )
 end
 
-def json_format(csv , json_file_path = "json/ase/ase_1.json", count = 0)
-  # puts JSON.parse(File.open(json_file_path).read)
+def json_format(csv , json_file_path = "json/fse/fse_6.json", count = 0)
   JSON.parse(File.open(json_file_path).read).each do |hash| #open json to parse
     count = count + 1
-    puts "#{hash.values} ----"
-    if hash.values[18].include?("通年")
-      # 
-      puts "hash.values[18].include?(\"通年\")を通ってますね"
+    if hash.values[19][0].empty?
+      # たまにある授業時間割がない場合
+      csv << insert_element(hash.values, hash.values[18], 0, 0)
+    elsif hash.values[18].include?("通年")
+      k = 0
+      l = 0
+      hash.values[19][0].map do
+        csv << insert_element(hash.values, "春学期", 0, k)
+        puts "通年0 #{insert_element(hash.values, "春学期", 0, k)}"
+        k = k + 1
+      end
+      hash.values[19][1].map do
+        csv << insert_element(hash.values, "秋学期", 1, l)
+        puts "通年1 #{insert_element(hash.values, "秋学期", 1, l)}"
+        l = l + 1
+      end
     elsif hash.values[19][0].length > 1
-      # 通年以外(春か秋かのみ) かつ 2コマ以上 の場合
-      # ただし、微積などの場合は通年であり かつ 半期ごとに２コマ以上あるのでその場合は、注意する。
+      # 通年以外(春か秋かのみ) かつ 2コマ以上 の場合 ※ただし、微積などの場合は通年であり かつ 半期ごとに２コマ以上あるのでその場合は、注意する。
       next if hash.values[18].include?("通年")
       j = 0
       hash.values[19][0].map do
-        # csv << insert_element(hash.values, hash.values["term"], 0, j)
-        puts "insert_element(hash.values, hash.values[\"term\"], 0, j)  : #{insert_element(hash.values, hash.values[18], 0, j).class}"
+        puts "通年以外(春か秋かのみ) かつ 2コマ以上 の場合"
+        csv << insert_element(hash.values, hash.values[18], 0, j)
         j = j + 1
       end
     else
       puts "else文ですね"
-      # csv << insert_element(hash.values, hash.values["term"], 0, 0)
+      next if hash.values[18].include?("通年") || hash.values[19][0].length > 1
+      csv << insert_element(hash.values, hash.values[18], 0, 0)
     end
     puts "#{count}\n"
   end
