794a0f7d121ebf92d352a77371d8dddbac3081df
gomzaemon111 <goki14914@icloud.com>
2019-01-19 19:08:26 +0900

    add main2.rb

--- main2.rb
+++ main2.rb
@@ -1,71 +1,79 @@
 require "json"
 require "csv"
 
-csv_file_path = "csv/sss.csv"
+csv_file_path = "eva_csv/fse.csv"
 
-def insert_element(value, term, i, j)
+def eva_return(hash)
+  return "なし" if hash["evaluation_method"].nil?
+  return "なし" if hash["evaluation_method"].empty?
+
+  hash["evaluation_method"]
+end
+
+def insert_element(hash, term, i, j)
   # i = 通年かどうかで決まる 基本的に0のみ
   # j = その期で授業コマ文だけ回すため
-  day = !value[19][0].empty? ? value[19][i][j]["day"] : "なし"
-  period = !value[19][0].empty? ? value[19][i][j]["period"] : "なし"
-  classroom = !value[19][0].empty? ? value[19][i][j]["classroom"] : "なし"
-  [].push( value[0] ,
-           value[1] ,
-           value[2] ,
-           value[3] ,
-           value[4] ,
-           value[5] ,
-           value[6] ,
-           value[7] ,
-           value[8] ,
-           value[9] ,
-           value[10] ,
-           value[11] ,
-           value[12] ,
-           value[13] ,
-           value[14] ,
-           value[15] ,
-           value[16] ,
-           value[17] ,
+  day = !hash["metadata"][0].empty? ? hash["metadata"][i][j]["day"] : "なし"
+  period = !hash["metadata"][0].empty? ? hash["metadata"][i][j]["period"] : "なし"
+  classroom = !hash["metadata"][0].empty? ? hash["metadata"][i][j]["classroom"] : "なし"
+  evaluation_method = eva_return(hash)
+  [].push( hash["year"] ,
+           hash["place"] ,
+           hash["name"] ,
+           hash["professor"] ,
+           hash["time"] ,
+           hash["category"] ,
+           hash["target_grade"] ,
+           hash["credit"] ,
+           hash["classroom"] ,
+           hash["campus"] ,
+           hash["language"] ,
+           hash["field_large"] ,
+           hash["field_middle"] ,
+           hash["field_small"] ,
+           hash["level"] ,
+           hash["form"] ,
+           hash["open_course"] ,
+           hash["full_od"] ,
            term ,
            day ,
            period ,
            classroom,
-           value[20] )
+           hash["textbook"] || "なし",
+           evaluation_method )
 end
 
 def json_format(csv , json_file_path, count = 0)
   JSON.parse(File.open(json_file_path).read).each do |hash| #open json to parse
     count = count + 1
-    if hash.values[19][0].empty?
+    if hash["metadata"][0].empty?
       # たまにある授業時間割がない場合
-      csv << insert_element(hash.values, hash.values[18], 0, 0)
-    elsif hash.values[18].include?("通年")
+      csv << insert_element(hash, hash["term"], 0, 0)
+    elsif hash["term"].include?("通年")
       k = 0
       l = 0
-      hash.values[19][0].map do
-        csv << insert_element(hash.values, "春学期", 0, k)
-        puts "通年0 #{insert_element(hash.values, "春学期", 0, k)}"
+      hash["metadata"][0].map do
+        csv << insert_element(hash, "春学期", 0, k)
+        puts "通年0 #{insert_element(hash, "春学期", 0, k)}"
         k = k + 1
       end
-      hash.values[19][1].map do
-        csv << insert_element(hash.values, "秋学期", 1, l)
-        puts "通年1 #{insert_element(hash.values, "秋学期", 1, l)}"
+      hash["metadata"][1].map do
+        csv << insert_element(hash, "秋学期", 1, l)
+        puts "通年1 #{insert_element(hash, "秋学期", 1, l)}"
         l = l + 1
       end
-    elsif hash.values[19][0].length > 1
+    elsif hash["metadata"][0].length > 1
       # 通年以外(春か秋かのみ) かつ 2コマ以上 の場合 ※ただし、微積などの場合は通年であり かつ 半期ごとに２コマ以上あるのでその場合は、注意する。
-      next if hash.values[18].include?("通年")
+      next if hash["term"].include?("通年")
       j = 0
-      hash.values[19][0].map do
+      hash["metadata"][0].map do
         puts "通年以外(春か秋かのみ) かつ 2コマ以上 の場合"
-        csv << insert_element(hash.values, hash.values[18], 0, j)
+        csv << insert_element(hash, hash["term"], 0, j)
         j = j + 1
       end
     else
-      puts "else文ですね"
-      next if hash.values[18].include?("通年") || hash.values[19][0].length > 1
-      csv << insert_element(hash.values, hash.values[18], 0, 0)
+      next if hash["term"].include?("通年") || hash["metadata"][0].length > 1
+      csv << insert_element(hash, hash["term"], 0, 0)
     end
     puts "#{count}\n"
   end
@@ -73,8 +81,8 @@ end
 
 
 CSV.open(csv_file_path, "w") do |csv| #open new file for write
-  (1..6).each do |i|
-    json_format(csv, "json/sss/sss_#{i}.json")
+  (1..10).each do |i|
+    json_format(csv, "json/fse/fse_#{i}.json")
     puts "#{i}"
   end
 end
